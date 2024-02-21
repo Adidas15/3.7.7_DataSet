7a6ad4596434006eef82346a49464d467e938c0a
Adidas15 <adidasglhf@gmail.com>
2024-02-20 21:11:40 -0800

    Completed predicting for target year

--- CountryEm.java
+++ CountryEm.java
@@ -1,3 +1,44 @@
 public class CountryEm {
+
+    private String name;
+    private String abr;
+    private int year;
+    private double total;
+    private double coal;
+    private double oil;
+    private double gas;
+    private double cement;
+    private double flaring;
+    private double other;
+    private double perCapita;
     
+    public CountryEm(String name, String abr, int year, double total, double coal, double oil, double gas, double cement, double flaring, double other, double perCapita) {
+        this.name = name;
+        this.abr = abr;
+        this.year = year;
+        this.total = total;
+        this.coal = coal;
+        this.oil = oil;
+        this.gas = gas;
+        this.cement = cement;
+        this.flaring = flaring;
+        this.other = other;
+        this.perCapita = perCapita;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public boolean rightCountry(String c) {
+        return (name.equals(c) || abr.equals(c));
+    }
 }
--- Runner.java
+++ Runner.java
@@ -1,5 +1,54 @@
+import core.data.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
+        DataSource ds =DataSource.connectAs("CSV", "emissiondata.csv");
+        ds.load();
+        //ds.printUsageString();
+
+        ArrayList<CountryEm> allEntries = ds.fetchList("CountryEm", "Country", "ISO 3166-1 alpha-3", "Year", "Total", "Coal","Oil","Gas","Cement","Flaring","Other","Per Capita");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the country you want: ");
+        String country = sc.next();
+
+        ArrayList<CountryEm> specToCountry = new ArrayList<CountryEm>();
         
+        for (CountryEm cE : allEntries) {
+            if (cE.rightCountry(country) && cE.getTotal() > 0 && cE.getYear()>1975) {
+                specToCountry.add(cE);
+            }
+        }
+
+        ArrayList<Double> change = new ArrayList<Double>();
+        for (int i=0; i<specToCountry.size()-1;i++) {
+            //System.out.println(CE.getYear() + ": " + CE.getTotal());
+            change.add(specToCountry.get(i+1).getTotal() - specToCountry.get(i).getTotal());
+        }
+
+        double avgChange = ((change.get(change.size()-1))-change.get(0)) / change.size();
+        double avgIncreaseInChange = 0;
+        for (double ch : change) {
+            avgIncreaseInChange += ch;
+        }
+        avgIncreaseInChange /= change.size();
+
+        System.out.println("The average change is " + avgChange + " and the average increase in the change is " + avgIncreaseInChange);
+
+        System.out.println("What year do you want to predict?");
+        int predictYear = sc.nextInt();
+
+        double predictedForYear = specToCountry.get(specToCountry.size()-1).getTotal();
+
+        for (int y=0; y<predictYear-2020; y++) {
+            predictedForYear += avgChange + (avgIncreaseInChange*y);
+        }
+
+        System.out.println(predictedForYear + " million metric tons of CO2 is predicted to be released in the year " + predictYear);
+
+        
+
     }
 }
